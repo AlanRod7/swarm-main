93609fe51459fa6d7d0ee4252e464c1899fc772d
commodore_64 <cesarml2002@hotmail.com>
2024-03-30 20:17:55 -0600

    ACO front funcionando pero aun con duda

--- static/js/aco.js
+++ static/js/aco.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
     const ejecutarAco = document.getElementById('ejecutarAco');
     const formularioAco = document.getElementById('acoForm');
+    
 
     ejecutarAco.addEventListener('click', function () {
         console.log('ejecutarAco clicked');
@@ -12,33 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
         fetch('/aco', {
             method: 'POST',
             body: formData
+            
         })
         .then(response => response.json())  // Parsea la respuesta como JSON
         .then(data => {
             console.log('Datos recibidos:', data);
             
-            // Verificar si hay un error en los datos recibidos
-            if (data.error) {
-                console.error('Error en el servidor:', data.error);
-                return;
+            // Actualizar los campos de entrada con los nuevos datos
+            const mejoresAlternativas = data.mejor_alternativa;
+
+            for (let i = 0; i < mejoresAlternativas.length; i++) {
+                document.getElementById(`alternativa${i}`).innerText = mejoresAlternativas[i];
             }
 
-            // Verificar si 'mejor_alternativa' está definida y es un array
-            if (data.mejor_alternativa && Array.isArray(data.mejor_alternativa)) {
-                const mejoresAlternativas = data.mejor_alternativa;
-
-                for (let i = 0; i < mejoresAlternativas.length; i++) {
-                    document.getElementById(`alternativa${i}`).innerText = mejoresAlternativas[i];
-                }
-
-                document.getElementById('cantidadIteraciones').value = data.iteraciones;
-                document.getElementById('horaInicio').value = data.hora_inicio;
-                document.getElementById('fechaInicio').value = data.fecha_inicio;
-                document.getElementById('horaFinalizacion').value = data.hora_finalizacion;
-                document.getElementById('tiempoEjecucion').value = data.tiempo_ejecucion;
-            } else {
-                console.error('Datos de respuesta no válidos:', data);
-            }
+            document.getElementById('cantidadIteraciones').value = data.iteraciones;
+            document.getElementById('horaInicio').value = data.hora_inicio;
+            document.getElementById('fechaInicio').value = data.fecha_inicio;
+            document.getElementById('horaFinalizacion').value = data.hora_finalizacion;
+            document.getElementById('tiempoEjecucion').value = data.tiempo_ejecucion;
         })
         .catch(error => console.error('Error:', error));
     });
@@ -47,4 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
     formularioAco.addEventListener('submit', function (event) {
         event.preventDefault();
     });
+
+    
 });
